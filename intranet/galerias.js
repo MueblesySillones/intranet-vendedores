118ffd535a7391d15d77207021a47c8ae82aeb92
Muebles y Sillones <ezedigital2021@gmail.com>
2026-06-16 15:36:56 -0300

    Intranet de vendedores MyS: sitio listo para deploy

---
+++ intranet/galerias.js
@@ -0,0 +1,76 @@
+/* Generado automaticamente por actualizar_galerias.py. No editar a mano. */
+window.GALLERIES = {
+  "promos_bancarias": [
+    {
+      "file": "assets/promos_bancarias/Promociones bancarias.jpeg",
+      "title": "Promociones bancarias",
+      "download": "Promociones bancarias.jpeg"
+    }
+  ],
+  "promos_mensuales": [
+    {
+      "file": "assets/promos_mensuales/Bahiut Mora.jpeg",
+      "title": "Bahiut Mora",
+      "download": "Bahiut Mora.jpeg"
+    },
+    {
+      "file": "assets/promos_mensuales/Bajo LED Mediterranea.jpeg",
+      "title": "Bajo LED Mediterranea",
+      "download": "Bajo LED Mediterranea.jpeg"
+    },
+    {
+      "file": "assets/promos_mensuales/Dune Extra.jpeg",
+      "title": "Dune Extra",
+      "download": "Dune Extra.jpeg"
+    },
+    {
+      "file": "assets/promos_mensuales/Esquinero Philadelphia.jpeg",
+      "title": "Esquinero Philadelphia",
+      "download": "Esquinero Philadelphia.jpeg"
+    },
+    {
+      "file": "assets/promos_mensuales/Mesa Boston.jpeg",
+      "title": "Mesa Boston",
+      "download": "Mesa Boston.jpeg"
+    },
+    {
+      "file": "assets/promos_mensuales/Mesa Bradley.jpeg",
+      "title": "Mesa Bradley",
+      "download": "Mesa Bradley.jpeg"
+    },
+    {
+      "file": "assets/promos_mensuales/Pompei.jpeg",
+      "title": "Pompei",
+      "download": "Pompei.jpeg"
+    },
+    {
+      "file": "assets/promos_mensuales/Sicilia.jpeg",
+      "title": "Sicilia",
+      "download": "Sicilia.jpeg"
+    },
+    {
+      "file": "assets/promos_mensuales/Sillon Cuore.jpeg",
+      "title": "Sillon Cuore",
+      "download": "Sillon Cuore.jpeg"
+    }
+  ],
+  "entregas": [
+    {
+      "file": "assets/entregas/Servicio de entregas integral.jpeg",
+      "title": "Servicio de entregas integral",
+      "download": "Servicio de entregas integral.jpeg"
+    }
+  ],
+  "fechas_especiales": [
+    {
+      "file": "assets/fechas_especiales/Dia del padre.jpg",
+      "title": "Dia del padre",
+      "download": "Dia del padre.jpg"
+    },
+    {
+      "file": "assets/fechas_especiales/Polo Hudson 20 de junio.jpeg",
+      "title": "Polo Hudson 20 de junio",
+      "download": "Polo Hudson 20 de junio.jpeg"
+    }
+  ]
+};
