b8bee9c121a1fde87a1858b7d552d4f8ad87f5bb
Muebles y Sillones <ezedigital2021@gmail.com>
2026-07-17 10:09:35 -0300

    Actualizar imagenes/galerias desde el panel

--- intranet/modulos.js
+++ intranet/modulos.js
@@ -101,9 +101,42 @@ window.MODULES = [
         },
         {
           "t": "separador"
+        },
+        {
+          "t": "titulo",
+          "nivel": "h1",
+          "html": "Fecha"
+        },
+        {
+          "t": "parrafo",
+          "html": "TEST #"
+        },
+        {
+          "t": "separador"
+        },
+        {
+          "t": "titulo",
+          "nivel": "h1",
+          "html": "FECHA"
+        },
+        {
+          "t": "parrafo",
+          "html": "TEST #"
+        },
+        {
+          "t": "separador"
+        },
+        {
+          "t": "titulo",
+          "nivel": "h1",
+          "html": "FECHA"
+        },
+        {
+          "t": "parrafo",
+          "html": "TEST #"
         }
       ],
-      "html": "<div class=\"db\"><div class=\"m-h\">17/07/2026</div></div><div class=\"db\"><p class=\"m-p\">No hay mas disponibles Lyon patas con forma solo quedan las de stock</p></div><div class=\"db\"><div class=\"note\" style=\"--nota-acc:var(--c-canning)\"><span class=\"ico\"><svg viewBox=\"0 0 24 24\"><polyline points=\"20 6 9 17 4 12\"/></svg></span><div class=\"nt\">Disponible a fabricar solo patas rectas</div></div></div><div class=\"db\"><div style=\"height:1px;background:var(--border)\"></div></div>"
+      "html": "<div class=\"db\"><div class=\"m-h\">17/07/2026</div></div><div class=\"db\"><p class=\"m-p\">No hay mas disponibles Lyon patas con forma solo quedan las de stock</p></div><div class=\"db\"><div class=\"note\" style=\"--nota-acc:var(--c-canning)\"><span class=\"ico\"><svg viewBox=\"0 0 24 24\"><polyline points=\"20 6 9 17 4 12\"/></svg></span><div class=\"nt\">Disponible a fabricar solo patas rectas</div></div></div><div class=\"db\"><div style=\"height:1px;background:var(--border)\"></div></div><div class=\"db\"><div class=\"m-h\">Fecha</div></div><div class=\"db\"><p class=\"m-p\">TEST #</p></div><div class=\"db\"><div style=\"height:1px;background:var(--border)\"></div></div><div class=\"db\"><div class=\"m-h\">FECHA</div></div><div class=\"db\"><p class=\"m-p\">TEST #</p></div><div class=\"db\"><div style=\"height:1px;background:var(--border)\"></div></div><div class=\"db\"><div class=\"m-h\">FECHA</div></div><div class=\"db\"><p class=\"m-p\">TEST #</p></div>"
     }
   },
   {
